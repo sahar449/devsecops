9a15650db9a06e44363b4b6bf3b31623bfea0e94
Sahar <saharr449@gmail.com>
2025-02-22 16:04:53 +0200

    ,

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,16 +5,3 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
-
-// src/test/java/com/example/AppTest.java
-package com.example;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class AppTest {
-    @Test
-    public void testApp() {
-        assertEquals("Hello, World!", "Hello, World!");
-    }
-}
